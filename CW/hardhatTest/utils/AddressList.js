97c5f61d48608fdd103cf7321be780e09c920198
tHeStRyNg <info@algorithmic.one>
2024-04-07 09:30:21 +0000

    added notes

--- CW/hardhatTest/utils/AddressList.js
+++ CW/hardhatTest/utils/AddressList.js
@@ -1,7 +1,17 @@
+// Docs Uniswap V2 - https://docs.uniswap.org/contracts/v2/reference/smart-contracts/factory
+// const addressFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"; // Verify here --> https://etherscan.io/address/0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f
+// Docs Uniswap V2 - https://docs.uniswap.org/contracts/v2/reference/smart-contracts/router-02
+// const addressRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"; // Verify here --> https://etherscan.io/address/0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
+// Etherscan on WETH - https://etherscan.io/address/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+// const addressFrom = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"; // WETH Token - Wrapped Ethereum - Token Contract
+// Etherscan on SUSHI - https://etherscan.io/address/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+// const addressTo = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2"; // SUSHI - Sushiswap Shushi Token Contract
+
 const addressFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 const addressRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 const addressFrom = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"; // WETH
 const addressTo = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2"; // SUSHI
+
 
 module.exports = {
   addressFactory,
